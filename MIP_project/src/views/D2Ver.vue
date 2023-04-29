f57b87a02a581f59cd3492c622563b5d1436731e
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-04-29 09:49:23 +0800

    123

--- MIP_project/src/views/D2Ver.vue
+++ MIP_project/src/views/D2Ver.vue
@@ -1,6 +1,7 @@
 <template>
 <h5> 2D 视图 开发中</h5>
 <a href="https://www.cornerstonejs.org/docs/category/getting-started">库访问连接</a>
+<a href="https://cn.vuejs.org/api/composition-api-lifecycle"> Vue 文档</a>
 </template>
 
 <script setup>
