c4d48a7f938f8ee4edad4194e16414d1a4e32990
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-05-07 22:51:37 +0800

    123

--- MIP_project/src/App.vue
+++ MIP_project/src/App.vue
@@ -6,13 +6,6 @@ import 'element-plus/theme-chalk/dark/css-vars.css'
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 
-// key is used to force update the component
-const comcount = ref(0)
-function update() {
-  comcount.value++
-  console.log('update')
-}
-
 </script> 
 
 <template>
@@ -22,18 +15,16 @@ function update() {
 
 <!-- add update function to d2ver and d3ver button -->
   <el-button type="primary" siza="small" plain @click="$router.push('/')">Go to Home</el-button>
-  <el-button type="primary" siza="small" plain @click="$router.push('/d2ver');update()">Go to D2Ver</el-button>
-  <el-button type="primary" siza="small" plain @click="$router.push('/d3ver');update()">Go to D3Ver</el-button>
-  <el-button type="primary" siza="small" plain @click="$router.push('/segment');update()">Go to Segment</el-button>
-  <el-button type="primary" siza="small" plain @click="$router.push('/table');update()">Go to table</el-button>
+  <el-button type="primary" siza="small" plain @click="$router.push('/d2ver');">Go to D2Ver</el-button>
+  <el-button type="primary" siza="small" plain @click="$router.push('/d3ver');">Go to D3Ver</el-button>
+  <el-button type="primary" siza="small" plain @click="$router.push('/segment');">Go to Segment</el-button>
+  <el-button type="primary" siza="small" plain @click="$router.push('/table');">Go to table</el-button>
   <el-button type="primary" siza="small" plain @click="toggleDark()">暗黑模式</el-button>
   
 
   <!-- 路由出口 -->
   <!-- 路由匹配到的组件将渲染在这里 -->
-  <router-view :key="comcount"></router-view>
 
-  <main>
-    <TheWelcome />
-  </main>
+      <router-view />
+
 </template>
