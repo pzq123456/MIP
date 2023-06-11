702e4eb93892d8d8479fb2f63356b2959db080db
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-06-11 23:23:59 +0800

    123

--- MIP_project/src/utils/SegmentationReader.js
+++ MIP_project/src/utils/SegmentationReader.js
@@ -1,16 +1,23 @@
 import nrrd from 'nrrd-js';
 import pako from 'pako';
-import readImageArrayBuffer from 'itk/readImageArrayBuffer';
-import writeArrayBuffer from 'itk/writeArrayBuffer';
-import config from 'itk/itkConfig';
+//从CDN使用ITK
+import readImageArrayBuffer from 'https://unpkg.com/browse/itk@14.1.1//readImageArrayBuffer.js';
+import writeArrayBuffer from 'https://unpkg.com/browse/itk@14.1.1/writeArrayBuffer.js';
 
-const pkgJSON = require('../../package.json'); // "itk": "^14.1.1",
-const itkVersion = pkgJSON.dependencies.itk.substring(1); // "14.1.1"
-config.itkModulesPath = 'https://unpkg.com/itk@' + itkVersion; // HACK to use ITK from CDN 从CDN使用ITK
+// import config from 'itk/itkConfig';
+// const pkgJSON = require('../../package.json'); // "itk": "^14.1.1",
+// const itkVersion = pkgJSON.dependencies.itk.substring(1); // "14.1.1"
+// config.itkModulesPath = 'https://unpkg.com/itk@' + itkVersion; // HACK to use ITK from CDN 
 
 
 export default class SegmentationReader {
+  /**
+   * Parse NRRD data
+   * @param {*} data
+   * @returns {header, image}
+   */
   static parseNrrdData(data) {
+
     let nrrdfile = nrrd.parse(data);
 
     // Currently gzip is not supported in nrrd.js
@@ -33,19 +40,29 @@ export default class SegmentationReader {
     };
   }
 
+  /**
+   * 保存文件
+   * @param {*} blob  
+   * @param {*} filename
+   * @returns
+   */
   static saveFile(blob, filename) {
     if (window.navigator.msSaveOrOpenBlob) {
+      // 如果是IE浏览器
       window.navigator.msSaveOrOpenBlob(blob, filename);
     } else {
+      // 考虑兼容问题，使用a标签下载
       const a = document.createElement('a');
       document.body.appendChild(a);
-
+      // 创建blob地址
       const url = window.URL.createObjectURL(blob);
       a.href = url;
       a.download = filename;
+      // 触发点击事件，然后移除
       a.click();
 
       setTimeout(() => {
+        // 延时释放
         window.URL.revokeObjectURL(url);
         document.body.removeChild(a);
       }, 0);
@@ -53,6 +70,12 @@ export default class SegmentationReader {
   }
 
   // GZIP write not supported by nrrd-js (so use ITK save with compressed = true)
+  /**
+   * 保存压缩文件
+   * @param {*} header 
+   * @param {*} image 
+   * @param {*} filename 
+   */
   static serializeNrrdCompressed(header, image, filename) {
     const nrrdBuffer = SegmentationReader.serializeNrrd(header, image);
 
@@ -71,6 +94,13 @@ export default class SegmentationReader {
     });
   }
 
+  /**
+   * serialize nrrd
+   * @param {*} header 
+   * @param {*} image 
+   * @param {*} filename 
+   * @returns 
+   */
   static serializeNrrd(header, image, filename) {
     let nrrdOrg = Object.assign({}, header);
     nrrdOrg.buffer = image;
