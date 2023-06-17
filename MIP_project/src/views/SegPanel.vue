7da168f36f368b2f005ea02d6fd6f6d922066e2e
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-06-17 22:40:44 +0800

    feat: SegPanel

---
+++ MIP_project/src/views/SegPanel.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div>
+        <p>
+            说明: 分割面板的开发测试页面，用于测试分割面板的功能。 <br>
+            测试面板的组件位于 "MIP_project\src\SegComs\coms" 目录下。 <br>
+            需要说明的是，"coms/actions" 文件家中存储的是模态对话框，用于输入信息。        
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
